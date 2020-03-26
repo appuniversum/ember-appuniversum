8e681e3866ebf9af90ff9636c9e3b679ed419686
Miet Claes <github@miet.be>
2020-03-26 12:31:38 +0100

    Start adding ember way of putting classes on components

---
+++ addon/components/au-textarea.js
@@ -0,0 +1,17 @@
+import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+
+export default class AuTextarea extends Component {
+
+  get widthClass() {
+      if (this.args.width == "block") {
+          return "au-c-textarea--block";
+     }
+  }
+
+  get errorClass() {
+    if( this.args.error == "error" ){
+      return "au-c-textarea--error";
+    }
+  }
+};
