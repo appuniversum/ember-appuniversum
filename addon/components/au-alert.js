aa27f712c91671728d17cd98f151e49644084c35
Miet Claes <github@miet.be>
2020-04-01 13:11:02 +0200

    Start adding alert component and styling

---
+++ addon/components/au-alert.js
@@ -0,0 +1,29 @@
+import Component from '@glimmer/component';
+
+export default class AuAlertComponent extends Component {
+  get alertSkin() {
+    if (this.args.alertskin == "info") {
+      return "--info";
+    }
+    if (this.args.alertskin == "success") {
+      return "--success";
+    }
+    if (this.args.alertskin == "warning") {
+      return "--warning";
+    }
+    if (this.args.alertskin == "error") {
+      return "--error";
+    }
+    return "";
+  }
+
+  get alertSize() {
+    if (this.args.alertsize == "tiny") {
+      return "--tiny";
+    }
+    if (this.args.alertsize == "small") {
+      return "--small";
+    }
+    return "";
+  }
+}
