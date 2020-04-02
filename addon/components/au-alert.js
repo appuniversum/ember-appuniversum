57786d25f2e718d63032e0617c217f937709008f
Miet Claes <github@miet.be>
2020-04-02 10:05:33 +0200

    Refactor and give correct classes

--- addon/components/au-alert.js
+++ addon/components/au-alert.js
@@ -2,28 +2,22 @@ import Component from '@glimmer/component';
 
 export default class AuAlertComponent extends Component {
   get alertSkin() {
-    if (this.args.alertskin == "info") {
+    if (this.args.alertskin == "info")
       return "--info";
-    }
-    if (this.args.alertskin == "success") {
+    if (this.args.alertskin == "success")
       return "--success";
-    }
-    if (this.args.alertskin == "warning") {
+    if (this.args.alertskin == "warning")
       return "--warning";
-    }
-    if (this.args.alertskin == "error") {
+    if (this.args.alertskin == "error")
       return "--error";
-    }
     return "";
   }
 
   get alertSize() {
-    if (this.args.alertsize == "tiny") {
+    if (this.args.alertsize == "tiny")
       return "--tiny";
-    }
-    if (this.args.alertsize == "small") {
+    if (this.args.alertsize == "small")
       return "--small";
-    }
     return "";
   }
 }
