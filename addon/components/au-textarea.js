3fd24e107876cc44618ac91e8e4ffbb2fc63b27b
Miet Claes <github@miet.be>
2020-04-02 12:23:46 +0200

    Improve textarea styles and functionalities

--- addon/components/au-textarea.js
+++ addon/components/au-textarea.js
@@ -3,14 +3,14 @@ import Component from '@glimmer/component';
 export default class AuTextarea extends Component {
   get widthClass() {
     if (this.args.width == "block")
-      return "au-c-input--block";
+      return "au-c-textarea--block";
     else
       return "";
   }
 
   get errorClass() {
     if (this.args.error == "error")
-      return "au-c-input--error";
+      return "au-c-textarea--error";
     else
       return "";
   }
