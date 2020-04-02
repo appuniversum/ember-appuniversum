e6eb49ef724c31937781a5b5bd50d48cf932394d
Miet Claes <github@miet.be>
2020-04-02 12:37:51 +0200

    Remove icon functionality to remove double classes

--- addon/components/au-alert.js
+++ addon/components/au-alert.js
@@ -3,21 +3,21 @@ import Component from '@glimmer/component';
 export default class AuAlertComponent extends Component {
   get alertSkin() {
     if (this.args.alertskin == "info")
-      return "--info";
+      return "au-c-alert--info";
     if (this.args.alertskin == "success")
-      return "--success";
+      return "au-c-alert--success";
     if (this.args.alertskin == "warning")
-      return "--warning";
+      return "au-c-alert--warning";
     if (this.args.alertskin == "error")
-      return "--error";
+      return "au-c-alert--error";
     return "";
   }
 
   get alertSize() {
     if (this.args.alertsize == "tiny")
-      return "--tiny";
+      return "au-c-alert--tiny";
     if (this.args.alertsize == "small")
-      return "--small";
+      return "au-c-alert--small";
     return "";
   }
 }
