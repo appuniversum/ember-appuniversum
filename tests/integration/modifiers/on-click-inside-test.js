61925a21bb78a7b834ad961a18471488d260ccf8
Dieter Peirs <github@dieterpeirs.com>
2020-04-08 16:54:01 +0200

    make dropdown close when an item is clicked

---
+++ tests/integration/modifiers/on-click-inside-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Modifier | on-click-inside', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function(assert) {
+    await render(hbs`<div {{on-click-inside}}></div>`);
+
+    assert.ok(true);
+  });
+});
