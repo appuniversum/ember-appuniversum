473b28bb8a78bae22f277ecdc10caf9c2ba7d523
Dieter Peirs <github@dieterpeirs.com>
2020-04-10 13:44:28 +0200

    add au-link component, remove au-button-link component

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -12,6 +12,7 @@ Router.map(function() {
       this.route('au-icon');
       this.route("au-button");
       this.route('au-input');
+      this.route('au-link');
       this.route("au-navigation-link");
       this.route('au-textarea');
     });
--- tests/integration/components/au-link-test.js
+++ tests/integration/components/au-link-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | au-button-link', function(hooks) {
+module('Integration | Component | au-link', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<AuButtonLink />`);
+    await render(hbs`<AuLink />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <AuButtonLink>
+      <AuLink>
         template block text
-      </AuButtonLink>
+      </AuLink>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
