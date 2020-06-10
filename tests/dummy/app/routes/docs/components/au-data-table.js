a939f7d7de7cf059a859f06e890f70909ef7b7da
Dieter Peirs <github@dieterpeirs.com>
2020-06-10 18:05:54 +0200

    replace data table json

--- tests/dummy/app/routes/docs/components/au-data-table.js
+++ tests/dummy/app/routes/docs/components/au-data-table.js
@@ -3,9 +3,20 @@ import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
 export default Route.extend(DataTableRouteMixin, {
   async model () {
-    let response = await fetch('/api/data-table-demo.json');
-    let parsed = await response.json();
-    return parsed;
+    return [
+      {
+        title: "Gemeenteraad Gent",
+        description: "Reglementen en verordeningen"
+      },
+      {
+        title: "Gemeenteraad Gent",
+        description: "Advies bij jaarrekening AGB"
+      },
+      {
+        title: "College van Burgemeester en Schepenen Gent",
+        description: "Besluitenlijst"
+      }
+    ]
   },
   queryParams: {
     sort: { refreshModel: true },
