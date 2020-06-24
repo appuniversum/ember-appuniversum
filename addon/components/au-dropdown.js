bacc72b41cb682362addf453f4f785c6636855fc
Dieter Peirs <github@dieterpeirs.com>
2020-06-24 15:08:14 +0200

    rework dropdown

--- addon/components/au-dropdown.js
+++ addon/components/au-dropdown.js
@@ -9,8 +9,6 @@ export default class AuDropdown extends Component {
 
   // Track dropdown state
   @tracked dropdownOpen = false;
-  // Check for keyboard access
-  @tracked keyboardAccess = false;
 
   // Open dropdown
   @action
@@ -23,49 +21,6 @@ export default class AuDropdown extends Component {
   closeDropdown() {
     // Toggle dropdown view state
     this.dropdownOpen = false;
-
-    // Reset keyboardAccess
-    this.keyboardAccess = false;
-  }
-
-  @action
-  escapeDropdown(event) {
-    // Toggle dropdown view state and enable keyboardAccess on escape keydown
-    if (event.keyCode === 27) {
-      this.dropdownOpen = false;
-      this.keyboardAccess = true;
-    }
-
-    // Enable keyboardAccess on tab keydown
-    if (event.keyCode === 9) {
-      this.keyboardAccess = true;
-    }
-  }
-
-  @action
-  tabDropdown(event) {
-    // Enable keyboardAccess on enter keydown
-    if (event.keyCode === 13) {
-      this.keyboardAccess = true;
-    }
-
-    // Enable keyboardAccess on space keydown
-    if (event.keyCode === 32) {
-      this.keyboardAccess = true;
-    }
-  }
-
-  @action
-  focusDropdown(element) {
-    let menuItems = element.querySelectorAll('[role="menuitem"]');
-
-    if (this.keyboardAccess) {
-      // Focus first button
-      menuItems[0].focus();
-    } else {
-      // Focus dropdown
-      element.focus();
-    }
   }
 
   // Dropdown alignment
