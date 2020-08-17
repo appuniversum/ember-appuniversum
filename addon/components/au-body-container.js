76ee94f6e2757b123ab77a75f86b1c9272196622
Miet <github@miet.be>
2020-08-17 17:38:28 +0200

    Correct body-container css scroll class

--- addon/components/au-body-container.js
+++ addon/components/au-body-container.js
@@ -3,7 +3,7 @@ import Component from '@glimmer/component';
 export default class AuBodyContainer extends Component {
   get scroll() {
     if (this.args.scroll == "true")
-      return "au-c-input--scroll";
+      return "au-c-body-container--scroll";
     else
       return "";
   }
