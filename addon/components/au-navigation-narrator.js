ed7d5e249bc4aa7bed6f09ea4ea4cbacaa99cd3a
Dieter Peirs <github@dieterpeirs.com>
2020-09-08 16:31:11 +0200

    replace a11y refocus

--- addon/components/au-navigation-narrator.js
+++ addon/components/au-navigation-narrator.js
@@ -1,5 +1,18 @@
-import NavigationNarrator from 'ember-a11y-refocus/components/navigation-narrator';
+import { alias } from '@ember/object/computed';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import Component from '@ember/component';
+import layout from 'a11y-announcer/templates/components/route-announcer';
 
-export default NavigationNarrator.extend({
-
+export default Component.extend({
+  announcer: service('announcer'),
+  layout: layout,
+  classNames: ['au-u-hidden-visually'],
+  attributeBindings: ['ariaLive:aria-live'],
+  ariaLive: computed('tone', function() {
+    return this.get('announcer').getWithDefault('announceTone', 'polite');
+  }),
+  tone: alias('announcer.announceTone'),
+  message: "Nieuwe pagina geladen"
+  // message: alias('announcer.announceMessage')
 });
