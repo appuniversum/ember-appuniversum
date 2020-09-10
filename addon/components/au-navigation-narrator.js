cb550b944e8773371fd18664519c08b806152b02
Dieter Peirs <github@dieterpeirs.com>
2020-09-10 12:10:53 +0200

    rework navigation narrator

--- addon/components/au-navigation-narrator.js
+++ addon/components/au-navigation-narrator.js
@@ -1,18 +1,10 @@
-import { alias } from '@ember/object/computed';
-import { computed } from '@ember/object';
-import { inject as service } from '@ember/service';
 import Component from '@ember/component';
-import layout from 'a11y-announcer/templates/components/route-announcer';
+import { inject as service } from '@ember/service';
 
-export default Component.extend({
-  announcer: service('announcer'),
-  layout: layout,
-  classNames: ['au-u-hidden-visually'],
-  attributeBindings: ['ariaLive:aria-live'],
-  ariaLive: computed('tone', function() {
-    return this.get('announcer').getWithDefault('announceTone', 'polite');
-  }),
-  tone: alias('announcer.announceTone'),
-  message: "Nieuwe pagina geladen"
-  // message: alias('announcer.announceMessage')
-});
+export default class AuNavigationNarrator extends Component {
+  @service router;
+
+  get activeRoute() {
+    return "Nieuwe pagina: " + this.router.currentRouteName ;
+  }
+}
