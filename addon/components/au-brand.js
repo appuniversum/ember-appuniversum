76e30fa9d9c7b87439e5313097538139cb79a5be
Dieter Peirs <github@dieterpeirs.com>
2020-09-17 14:41:02 +0200

    fixes #42

--- addon/components/au-brand.js
+++ addon/components/au-brand.js
@@ -1,6 +1,9 @@
 import Component from '@glimmer/component';
+import { guidFor } from '@ember/object/internals';
 
 export default class AuBrand extends Component {
+  brandId = guidFor(this);
+
   get tagline() {
     if (this.args.tagline)
       return "au-c-brand--tagline";
