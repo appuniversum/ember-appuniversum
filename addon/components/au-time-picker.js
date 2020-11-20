3eb6910c41545e231834e47cf9b168724fcb163e
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-11-20 16:13:25 +0100

    linting

--- addon/components/au-time-picker.js
+++ addon/components/au-time-picker.js
@@ -6,93 +6,95 @@ export default class AuTimePickerComponent extends Component {
   @tracked hourValue = 12;
   @tracked minuteValue = 0;
   @tracked secondValue = 0;
-  @tracked keyCodes = [ 8 , 9, 13 , 33 , 34 , 37 , 39, 46 ]
+  @tracked keyCodes = [ 8 , 9, 13 , 33 , 34 , 37 , 39, 46 ];
 
-  @action increment(elem){ 
-
+  @action
+  increment(elem){
     if(elem == "hourValue"){
-      ++this[elem]
+      ++this[elem];
 
       if(this[elem] >= 24){
-        this[elem] = 24
+        this[elem] = 24;
       }
     }
 
     if(elem != "hourValue"){
-      ++this[elem]
+      ++this[elem];
 
       if(this[elem] >= 60){
-        this[elem] = 60
+        this[elem] = 60;
       }
     }
   }
 
 
-  @action decrement(elem){
+  @action
+  decrement(elem){
 
     if(elem == "hourValue"){
-      --this[elem]
+      --this[elem];
 
       if(this[elem] <= 1){
-        this[elem] = 1
+        this[elem] = 1;
       }
     }
 
     if(elem != "hourValue"){
-      --this[elem]
+      --this[elem];
 
       if(this[elem] <= 0){
-        this[elem] = 0
+        this[elem] = 0;
       }
     }
   }
 
   // Keyboard input
-
-  @action setTimeValue(elem, e){
+  @action
+  setTimeValue(elem, e){
 
     if( e.keyCode == 38 ){
-      this.increment(elem)
+      this.increment(elem);
 
     } else if (e.keyCode == 40){
-      this.decrement(elem)
+      this.decrement(elem);
 
     } else if(isNaN(parseFloat(e.key)) && this.keyCodes.indexOf(e.keyCode) == -1 ){
       e.preventDefault();
 
     } else if(e.target.value.length >= 2 && this.keyCodes.indexOf(e.keyCode) == -1) {
-      e.preventDefault()
-
-    } 
+      e.preventDefault();
+    }
   }
 
-  @action updateTime(elem, e){
-    let inputValue = e.target.value
+  @action
+  updateTime(elem, e){
+    let inputValue = e.target.value;
     if(elem == "hourValue"){
       if(inputValue < 0){
-        this[elem] = 0
+        this[elem] = 0;
       } else if ( inputValue > 24 ) {
-        this[elem] = 24
+        this[elem] = 24;
       } else {
-        this[elem] = inputValue
+        this[elem] = inputValue;
       }
     }
 
     if(elem != "hourValue"){
       if(inputValue < 0){
-        this[elem] = 0
+        this[elem] = 0;
       } else if ( inputValue > 60 ) {
-        this[elem] = 60
+        this[elem] = 60;
       } else {
-        this[elem] = inputValue
+        this[elem] = inputValue;
       }
     }
   }
 
-  @action setCurrentTime(){
-    let current = new Date()
-    this.hourValue = current.getHours()
-    this.minuteValue = current.getMinutes()
-    this.secondValue = current.getSeconds()
+  @action
+  setCurrentTime(){
+    let current = new Date();
+    this.hourValue = current.getHours();
+    this.minuteValue = current.getMinutes();
+    this.secondValue = current.getSeconds();
   }
 }
