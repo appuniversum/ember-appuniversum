db2831903921ec827b9c6a7a67831b5e13ce35f8
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-03-03 13:24:08 +0100

    Don't lint the table components
    These are based on the classic code style from ember-data-table which triggers a lot of linting errors.

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane'
+  extends: 'octane',
+  ignore: [
+    'addon/components/au-data-table.hbs',
+    'addon/components/au-data-table/**'
+  ],
 };
