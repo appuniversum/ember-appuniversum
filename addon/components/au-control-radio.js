0ebe3c50cb70051dbb39358c346c591fefac6ecd
Niels Vandekeybus <progster@gmail.com>
2021-03-04 17:17:14 +0100

    allow an onchange action to be set on a radio button
    fixes #46

--- addon/components/au-control-radio.js
+++ addon/components/au-control-radio.js
@@ -1,4 +1,5 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class AuControlRadio extends Component {
   get disabled() {
@@ -6,5 +7,12 @@ export default class AuControlRadio extends Component {
       return "is-disabled";
     else
       return "";
+  }
+
+  @action
+  onChange(event) {
+    if (this.args.onChange) {
+      this.args.onChange(event.target.value);
+    }
   }
 }
