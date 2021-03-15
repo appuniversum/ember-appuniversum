2937a43f90fdf5f137b0461983ca8069d779fa51
Dieter Peirs <github@dieterpeirs.com>
2021-03-15 15:27:46 +0100

    add checkbox functionality

---
+++ addon/components/au-toggle-switch.js
@@ -0,0 +1,18 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class AuToggleSwitch extends Component {
+  get disabled() {
+    if (this.args.disabled)
+      return "is-disabled";
+    else
+      return "";
+  }
+
+  @action
+  onChange(event) {
+    if (this.args.onChange) {
+      this.args.onChange(event.target.checked, event);
+    }
+  }
+}
