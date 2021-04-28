f818fea18a4a21c765c45ed4816706fa1bc13a4f
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-04-28 20:49:11 +0200

    Prevent the closeModal callback from running twice

--- addon/components/au-modal.js
+++ addon/components/au-modal.js
@@ -34,11 +34,16 @@ export default class AuModal extends Component {
     });
 
   }
+
   @action
   closeModal() {
-    this.setInert(true);
-    this.args.closeModal && this.args.closeModal();
+    // Don't run the callback when the modal is already closed.
+    // {{focus-trap}} calls closeModal when it is deactivated which also happens when the element it is attached to is destroyed.
+    // This means that this method will be called twice if the user closes the modal with something else than the escape button.
+    // (once by the click handler of the button and once when {{focus-trap}} deactivates)
+    if (this.args.modalOpen) {
+      this.setInert(true);
+      this.args.closeModal?.();
+    }
   }
-
-
 }
