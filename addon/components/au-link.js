9332b1cd4236c4dc2d063d26581da630244748bb
Brian Gantzler <brian.gantzler@cardinalhealth.com>
2021-05-12 13:15:59 -0400

    Update link to for embroider

--- addon/components/au-link.js
+++ addon/components/au-link.js
@@ -1,6 +1,12 @@
 import Component from '@glimmer/component';
+import linkComponent from '@ember/routing/link-component';
+import { ensureSafeComponent } from '@embroider/util';
 
 export default class AuLink extends Component {
+  get linkTo() {
+    return ensureSafeComponent(linkComponent, this);
+  }
+
   get skin() {
     if (this.args.skin == "secondary")
       return "au-c-link--secondary";
