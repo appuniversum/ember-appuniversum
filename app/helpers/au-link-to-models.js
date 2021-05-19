1e45c6e0bd10a69bf280ee28029395e619804cb8
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-05-19 12:32:05 +0200

    Create a new `au-link-to-models` helper
    This helper can be used to support both `@model` and `@models` arguments when wrapping the `<LinkTo>` component.
    This is needed because the `<LinkTo>` component triggers an assertion if both are provided at the same time, even if they are `undefined`.

---
+++ app/helpers/au-link-to-models.js
@@ -0,0 +1,1 @@
+export { default } from '@appuniversum/ember-appuniversum/helpers/au-link-to-models';
---
+++ tests/unit/helpers/au-link-to-models-test.js
@@ -0,0 +1,50 @@
+import { auLinkToModels } from '@appuniversum/ember-appuniversum/helpers/au-link-to-models';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Helper | au-link-to-models', function(hooks) {
+  setupTest(hooks);
+
+  test('it supports passing in a model as the first positional argument', function(assert) {
+    let modelId = 1;
+    let result = auLinkToModels([modelId]);
+
+    assert.ok(Array.isArray(result));
+    assert.equal(result[0], 1);
+  });
+
+  test('it supports passing in a models array as the second positional argument', function(assert) {
+    let models = [1, 2, 3];
+    let result = auLinkToModels([undefined, models]);
+
+    assert.ok(Array.isArray(result));
+    assert.equal(result[0], 1);
+    assert.equal(result[1], 2);
+    assert.equal(result[2], 3);
+  });
+
+  test('it passes through models as provided so the `<LinkTo>` component validation can be reused', function(assert) {
+    let models = 1;
+    let result = auLinkToModels([undefined, models]);
+
+    assert.notOk(Array.isArray(result));
+    assert.equal(result, 1);
+  });
+
+  test('it returns an empty array if no model or models are provided', function(assert) {
+    let result = auLinkToModels([]);
+
+    assert.ok(Array.isArray(result));
+    assert.equal(result.length, 0);
+  });
+
+  test('it throws an assertion if both model and models is provided', function(assert) {
+    let result;
+
+    assert.throws(() => {
+      result = auLinkToModels([1, [1, 2]]);
+    })
+
+    assert.notOk(result);
+  });
+});
