cb6bcc384bba8e5a9fa4108af745a68057a583ea
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-06-08 18:17:06 +0200

    Add button skins to the `<AuLink>` component
    This makes it possible to style links as buttons by using the `button` or `button-secondary` skins.

--- addon/components/au-link.js
+++ addon/components/au-link.js
@@ -1,17 +1,25 @@
 import Component from '@glimmer/component';
 
+const SKIN_CLASSES = {
+  primary: 'au-c-link',
+  secondary: 'au-c-link au-c-link--secondary',
+  button: 'au-c-button',
+  'button-link': 'au-c-button au-c-button-secondary',
+};
+
 export default class AuLink extends Component {
   get skin() {
-    if (this.args.skin == "secondary")
-      return "au-c-link--secondary";
-    else
-      return "";
+    if (SKIN_CLASSES[this.args.skin]) {
+      return SKIN_CLASSES[this.args.skin];
+    } else {
+      return SKIN_CLASSES.primary;
+    }
   }
 
   get active() {
     if (this.args.active)
-      return "is-active";
+      return 'is-active';
     else
-      return "";
+      return '';
   }
 }
