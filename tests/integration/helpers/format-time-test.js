ac0621cfdf3f5b122602e7f53f993a9ec8bc5226
Karel Persoons <karel@brenner.company>
2021-11-05 16:50:50 +0100

    Rename helper (to avoid conflicts with other libraries/plugins)

--- tests/integration/helpers/format-time-test.js
+++ tests/integration/helpers/format-time-test.js
@@ -3,14 +3,14 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Helper | format-time', function(hooks) {
+module('Integration | Helper | format-time-digit', function(hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
   test('it renders', async function(assert) {
     this.set('inputValue', '1234');
 
-    await render(hbs`{{format-time inputValue}}`);
+    await render(hbs`{{format-time-digit inputValue}}`);
 
     assert.dom(this.element).hasText('1234');
   });
