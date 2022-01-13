5f7c3248710b45d98ee60dc2eefb75fe2caeeb8d
Dieter Peirs <github@dieterpeirs.com>
2022-01-13 12:37:39 +0100

    style storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import vlTheme from './vlTheme';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,5 +8,25 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  layout: 'centered'
+  layout: 'centered',
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'white',
+        value: '#FFFFFF',
+      },
+      {
+        name: 'light',
+        value: '#F4F5F6',
+      },
+      {
+        name: 'dark',
+        value: '#212326',
+      },
+    ],
+  },
+  docs: {
+    theme: vlTheme,
+  }
 }
