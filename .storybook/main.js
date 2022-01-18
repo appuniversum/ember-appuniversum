030c11224bc61f636e7b1f3d906132e348dab6f5
Dieter Peirs <github@dieterpeirs.com>
2022-01-18 14:37:34 +0100

    add story addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-a11y"
+    "@storybook/addon-a11y",
+    "@storybook/addon-storysource"
   ],
   staticDirs: ['../dist', '../stories/assets']
 }
