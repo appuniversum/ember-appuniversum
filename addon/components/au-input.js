d3fd331d65bf697bf373cec938f31ef68080b0e6
Dieter Peirs <github@dieterpeirs.com>
2022-02-08 14:45:18 +0100

    fix mistakes in input mask

--- addon/components/au-input.js
+++ addon/components/au-input.js
@@ -26,12 +26,4 @@ export default class AuInput extends Component {
     if (this.args.disabled) return 'is-disabled';
     else return '';
   }
-
-  get mask() {
-    return this.args.mask;
-  }
-
-  get maskPlaceholder() {
-    return this.args.maskPlaceholder;
-  }
 }
