3c86cd9bbad67802ffda1a95a4f5eca99b715af8
Dieter Peirs <github@dieterpeirs.com>
2022-03-28 14:57:38 +0200

    refactor pill, add options: new skins, route and href, pill action

--- stories/4-components/Notifications/AuPill.stories.js
+++ stories/4-components/Notifications/AuPill.stories.js
@@ -7,8 +7,12 @@ export default {
     text: { control: 'text', description: 'Sets the style of the pill' },
     skin: {
       control: 'select',
-      options: ['default', 'border', 'action', 'success', 'warning', 'error'],
-      description: 'Adds an icon',
+      options: ['default', 'border', 'ongoing', 'link', 'success', 'warning', 'error'],
+      description: 'Sets the skin of the pill.',
+    },
+    draft: {
+      control: 'boolean',
+      description: 'Applies a dashed line to the pill skin',
     },
     icon: {
       control: 'select',
@@ -21,6 +25,31 @@ export default {
       description:
         'Choose the position of the icon, adds correct margin next to the icon',
     },
+    hideText: {
+      control: 'boolean',
+      description: 'Hides the text visually',
+    },
+    route: {
+      control: 'text',
+      description: 'Add a route to the link component',
+    },
+    href: {
+      control: 'text',
+      description: 'Add a href to the link component',
+    },
+    action: {
+      control: 'text',
+      description: 'Adds an action to the pill',
+    },
+    actionIcon: {
+      control: 'select',
+      options: icons,
+      description: 'Sets the icon for the action',
+    },
+    actionText: {
+      control: 'text',
+      description: 'Description of the action for screenreaders',
+    }
   },
   parameters: {
     layout: 'padded',
@@ -31,8 +60,15 @@ const Template = (args) => ({
   template: hbs`
     <AuPill
       @skin={{this.skin}}
+      @draft={{this.draft}}
       @icon={{this.icon}}
       @iconAlignment={{this.iconAlignment}}
+      @hideText={{this.hideText}}
+      @route={{this.route}}
+      @href={{this.href}}
+      @action={{this.action}}
+      @actionIcon={{this.actionIcon}}
+      @actionText={this.actionText}
     >
       {{text}}
     </AuPill>`,
@@ -43,6 +79,13 @@ export const Component = Template.bind({});
 Component.args = {
   text: 'Pill',
   skin: '',
+  draft: false,
   icon: 'info-circle',
   iconAlignment: 'left',
+  hideText: false,
+  href: '',
+  route: '',
+  action: '',
+  actionIcon:'cross',
+  actionText: 'close',
 };
