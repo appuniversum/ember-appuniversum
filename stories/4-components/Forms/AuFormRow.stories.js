946ac01868dcdca3e5ff199d410a4e336c4d7d39
Dieter Peirs <github@dieterpeirs.com>
2022-04-11 17:41:12 +0200

    add form-row component, add basic form examples

---
+++ stories/4-components/Forms/AuFormRow.stories.js
@@ -0,0 +1,34 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+export default {
+  title: 'Components/Forms/AuFormRow',
+  argTypes: {
+    alignment: {
+      control: 'select',
+      options: ['default', 'inline','pre','post'],
+      description: 'Choose the layout of the form row.',
+    },
+  },
+  parameters: {
+    layout: 'padded',
+  },
+};
+
+const Template = (args) => ({
+  template: hbs`
+    <AuFormRow
+      @alignment={{this.alignment}}
+    >
+      <AuLabel for="input">
+        Label
+      </AuLabel>
+      <AuInput id="input" />
+    </AuFormRow>
+    `,
+  context: args,
+});
+
+export const Component = Template.bind({});
+Component.args = {
+  alignment: 'default',
+};
--- stories/5-patterns/forms.stories.js
+++ stories/5-patterns/forms.stories.js
@@ -3,28 +3,123 @@ import { hbs } from 'ember-cli-htmlbars';
 export default {
   title: 'Patterns/Forms',
   parameters: {
-    layout: 'fullscreen',
+    layout: 'padded',
   },
 };
 
 const Template = (args) => ({
   template: hbs`
-    <AuApp>
-      <AuMainContainer as |m|>
-        <m.content @scroll={{true}}>
-          <section class="au-o-region-large">
-            <AuForm as |form|>
-              <form::section>
-              </form::section>
-
-            </AuForm>
-          </section>
-        </m.content>
-      </AuMainContainer>
-    </AuApp>
+    <form class="au-c-form">
+      <AuFormRow @alignment="default">
+        <AuLabel for="input" @required={{true}} @requiredLabel="Required">
+          Title
+        </AuLabel>
+        <AuInput id="input" @width="block" />
+      </AuFormRow>
+      <AuFormRow @alignment="default">
+        <AuLabel for="inputTwo">
+          Date
+        </AuLabel>
+        <AuDatePicker id="inputTwo" />
+      </AuFormRow>
+      <AuFormRow @alignment="default">
+        <AuLabel for="textarea">
+          Textarea
+        </AuLabel>
+        <AuTextarea id="textarea" @width="block"></AuTextarea>
+      </AuFormRow>
+      <AuFormRow>
+        <AuButtonGroup>
+          <AuButton>
+            Submit
+          </AuButton>
+          <AuButton @skin="tertiary">
+            Cancel
+          </AuButton>
+        </AuButtonGroup>
+      </AuFormRow>
+    </form>
     `,
   context: args,
 });
 
-export const Page = Template.bind({});
-Page.args = {};
+const TemplateInline = (args) => ({
+  template: hbs`
+    <form class="au-c-form">
+      <AuFormRow @alignment="inline">
+        <AuLabel for="input" @required={{true}} @inline={{true}} class="au-u-1-6">
+          Title
+        </AuLabel>
+        <AuInput id="input" />
+      </AuFormRow>
+      <AuFormRow @alignment="inline">
+        <AuLabel for="inputTwo" @inline={{true}} class="au-u-1-6">
+          Date
+        </AuLabel>
+        <AuDatePicker id="inputTwo" />
+      </AuFormRow>
+      <AuFormRow @alignment="inline">
+        <AuLabel for="textarea" @inline={{true}} class="au-u-1-6">
+          Textarea
+        </AuLabel>
+        <AuTextarea id="textarea"></AuTextarea>
+      </AuFormRow>
+      <AuFormRow>
+        <AuButtonGroup>
+          <AuButton>
+            Submit
+          </AuButton>
+          <AuButton @skin="tertiary">
+            Cancel
+          </AuButton>
+        </AuButtonGroup>
+      </AuFormRow>
+    </form>
+    `,
+  context: args,
+});
+
+const TemplatePre = (args) => ({
+  template: hbs`
+    <form class="au-c-form">
+      <AuFormRow @alignment="pre">
+        <AuLabel for="input" @required={{true}} @inline={{true}} class="au-u-1-6">
+          Title
+        </AuLabel>
+        <AuInput id="input" />
+      </AuFormRow>
+      <AuFormRow @alignment="pre">
+        <AuLabel for="inputTwo" @inline={{true}} class="au-u-1-6">
+          Date
+        </AuLabel>
+        <AuDatePicker id="inputTwo" />
+      </AuFormRow>
+      <AuFormRow @alignment="pre">
+        <AuLabel for="textarea" @inline={{true}} class="au-u-1-6">
+          Textarea
+        </AuLabel>
+        <AuTextarea id="textarea"></AuTextarea>
+      </AuFormRow>
+      <AuFormRow>
+        <AuButtonGroup>
+          <AuButton>
+            Submit
+          </AuButton>
+          <AuButton @skin="tertiary">
+            Cancel
+          </AuButton>
+        </AuButtonGroup>
+      </AuFormRow>
+    </form>
+    `,
+  context: args,
+});
+
+export const SimpleForm = Template.bind({});
+SimpleForm.args = {};
+
+export const InlineForm = TemplateInline.bind({});
+InlineForm.args = {};
+
+export const PreForm = TemplatePre.bind({});
+PreForm.args = {};
