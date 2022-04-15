358e47eaab6b5a39973fb306ed93781a9023be4d
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-04-15 11:33:46 +0200

    Resolve ember-modifier 3.2 deprecations
    More information: https://github.com/ember-modifier/ember-modifier/blob/v3/MIGRATIONS.md#migrations

--- addon/modifiers/on-click-inside.js
+++ addon/modifiers/on-click-inside.js
@@ -1,15 +1,18 @@
 import { modifier } from 'ember-modifier';
 
-export default modifier(function onClickInside(element, [callback]) {
-  function handleClick(event) {
-    if (element.contains(event.target)) {
-      callback();
+export default modifier(
+  function onClickInside(element, [callback]) {
+    function handleClick(event) {
+      if (element.contains(event.target)) {
+        callback();
+      }
     }
-  }
 
-  document.addEventListener('click', handleClick);
+    document.addEventListener('click', handleClick);
 
-  return () => {
-    document.removeEventListener('click', handleClick);
-  };
-});
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  },
+  { eager: false }
+);
