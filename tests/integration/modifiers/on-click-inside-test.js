f7a199ed8e0692ccccd228bfba76750a25d59eab
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-04-15 11:10:33 +0200

    Add tests for the `on-click-inside` modifier

--- tests/integration/modifiers/on-click-inside-test.js
+++ tests/integration/modifiers/on-click-inside-test.js
@@ -1,15 +1,39 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Modifier | on-click-inside', function (hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function (assert) {
-    await render(hbs`<div {{on-click-inside}}></div>`);
+  test('it calls the provided function when clicking the element or one of its children', async function (assert) {
+    this.handleClick = () => {
+      assert.step('clicked');
+    };
 
-    assert.ok(true);
+    await render(hbs`
+      <div {{on-click-inside this.handleClick}}>
+        <button type="button" data-test-inside-button>Inside</button>
+      </div>
+      <button type="button" data-test-outside-button>Outside</button>
+    `);
+
+    await click('[data-test-inside-button]');
+    assert.verifySteps(
+      ['clicked'],
+      'clicking a child element triggers the callback'
+    );
+
+    await click('[data-test-outside-button]');
+    assert.verifySteps(
+      [],
+      "clicking an outside element doesn't trigger the callback"
+    );
+
+    await click('div');
+    assert.verifySteps(
+      ['clicked'],
+      'clicking the element itself also calls the callback'
+    );
   });
 });
