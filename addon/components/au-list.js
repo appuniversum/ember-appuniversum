ad8433cad144f5a6def8649dfabfbdbf2ebaa4c5
Dieter Peirs <github@dieterpeirs.com>
2022-04-19 15:52:53 +0200

    fix list component

--- addon/components/au-list.js
+++ addon/components/au-list.js
@@ -3,8 +3,7 @@ import Component from '@glimmer/component';
 export default class AuList extends Component {
   get direction() {
     if (this.args.direction == 'horizontal') return 'au-c-list--horizontal';
-    if (this.args.direction == 'vertical') return 'au-c-list--vertical';
-    else return '';
+    else return 'au-c-list--vertical';
   }
 
   get divider() {
