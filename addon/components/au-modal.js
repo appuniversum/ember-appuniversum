5b81e78109b10514a3403b59c3ea4a09c8d0848b
Dieter Peirs <github@dieterpeirs.com>
2022-06-03 11:43:04 +0200

    apply modal fix

--- addon/components/au-modal.js
+++ addon/components/au-modal.js
@@ -53,18 +53,7 @@ export default class AuModal extends Component {
 
   @action
   closeModal() {
-    // Prevent that the @closeModal action is run more than once.
-    // {{focus-trap}} calls closeModal when it is deactivated which also happens when the element it is attached to is destroyed.
-    // This means that this method will be called twice if the user closes the modal with something else than the escape button.
-    // (once by the click handler of the close button and once when {{focus-trap}} deactivates)
-    // It would be better if focus-trap exposed events that would be called when Escape is pressed, or the user clicks outside of the trap
-    // That way we replace `onDeactivate` with those events and this check wouldn't be needed anymore.
-    //
-    // More information: https://github.com/josemarluedke/ember-focus-trap/issues/36#issuecomment-850844483
-    // new focus-trap events issue: https://github.com/focus-trap/focus-trap/issues/126
-    if (!this.isDestroying && this.args.modalOpen) {
-      this.setInert(true);
-      this.args.closeModal?.();
-    }
+    this.setInert(true);
+    this.args.closeModal?.();
   }
 }
