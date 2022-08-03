50f29e70c382235e9741fec93507f838b964d068
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-08-03 18:08:21 +0200

    Remove support for Ember 3.20 and 3.24
    Ember 3.24 is EOL as of March 10, 2022.

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,18 +7,10 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.20',
+        name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
-            'ember-source': '~3.20.5',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3',
+            'ember-source': '~3.28.0',
           },
         },
       },
