cbb670dd3e92b16d29dbb6696c1414e4776f5120
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-09-13 17:16:09 +0200

    Add a route name where needed
    Using an empty string as the route name would actually make the getter return `undefined` which seems to result in a href of `#`. Now that we removed the getter the empty string is forwarded directly to the LinkTo component which makes it output no `href` at all. This affects the styling.

--- stories/5-components/Brand/AuMainFooter.stories.js
+++ stories/5-components/Brand/AuMainFooter.stories.js
@@ -18,10 +18,10 @@ const Template = (args) => ({
         <p>Uitgegeven door <a class="au-c-link" href="https://www.vlaanderen.be/organisaties/administratieve-diensten-van-de-vlaamse-overheid/beleidsdomein-kanselarij-en-bestuur/agentschap-binnenlands-bestuur">Agentschap Binnenlands Bestuur</a></p>
         <ul class="au-c-list-horizontal">
           <li class="au-c-list-horizontal__item">
-            <AuLink @route="" @skin="secondary">Disclaimer</AuLink>
+            <AuLink @route="index" @skin="secondary">Disclaimer</AuLink>
           </li>
           <li class="au-c-list-horizontal__item">
-            <AuLink @route="" @skin="secondary">Cookieverklaring</AuLink>
+            <AuLink @route="index" @skin="secondary">Cookieverklaring</AuLink>
           </li>
         </ul>
       </AuContent>
