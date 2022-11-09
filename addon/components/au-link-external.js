c324256abf0cc86426eb5f37395a90377a7dac94
Karel Persoons <karel@brenner.company>
2022-11-09 18:23:52 +0100

    Add button skins to external link component, rename getters & add missing active class getter

--- addon/components/au-link-external.js
+++ addon/components/au-link-external.js
@@ -3,10 +3,13 @@ import Component from '@glimmer/component';
 const SKIN_CLASSES = {
   primary: 'au-c-link',
   secondary: 'au-c-link au-c-link--secondary',
+  button: 'au-c-button au-c-button--primary',
+  'button-secondary': 'au-c-button au-c-button--secondary',
+  'button-naked': 'au-c-button au-c-button--naked',
 };
 
 export default class AuLinkExternal extends Component {
-  get skin() {
+  get skinClass() {
     if (SKIN_CLASSES[this.args.skin]) {
       return SKIN_CLASSES[this.args.skin];
     } else {
@@ -14,13 +17,28 @@ export default class AuLinkExternal extends Component {
     }
   }
 
-  get width() {
-    if (this.args.width == 'block') return 'au-c-link--block';
+  get widthClass() {
+    if (this.args.width == 'block')
+      if (this.args.skin.startsWith('button')) return 'au-c-button--block';
+      else return 'au-c-link--block';
+    else return '';
+  }
+
+  get activeClass() {
+    if (this.args.active) return 'is-active';
     else return '';
   }
 
   get iconAlignment() {
     if (this.args.iconAlignment) return this.args.iconAlignment;
     else return 'left';
+  }
+
+  get iconOnlyClass() {
+    if (this.args.icon && this.args.hideText)
+      if (this.args.skin && this.args.skin.startsWith('button'))
+        return 'au-c-button--icon-only';
+      else return 'au-c-link--icon-only';
+    return '';
   }
 }
