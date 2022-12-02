86f7306aa5f0edf9a7d7675bb62eb687361717c0
Karel Persoons <karel@brenner.company>
2022-12-02 18:11:28 +0100

    Add functionality for the example scroller component

--- tests/dummy/app/components/au-example-scroller.js
+++ tests/dummy/app/components/au-example-scroller.js
@@ -1,4 +1,31 @@
 import Component from '@glimmer/component';
+import { modifier } from 'ember-modifier';
 
-// eslint-disable-next-line ember/no-empty-glimmer-component-classes
-export default class AuExampleScroller extends Component {}
+export default class AuExampleScroller extends Component {
+  exampleScrollerModifier = modifier(
+    (element) => {
+      const child = element.children[0];
+
+      const scrollerDimensions = {
+        width: element.offsetWidth,
+        height: element.offsetHeight,
+      };
+
+      const childDimensions = {
+        width: child.offsetWidth,
+        height: child.offsetHeight,
+      };
+
+      if (childDimensions.width > scrollerDimensions.width) {
+        element.scrollLeft =
+          (childDimensions.width - scrollerDimensions.width) / 2;
+      }
+
+      if (childDimensions.height > scrollerDimensions.height) {
+        element.scrollTop =
+          (childDimensions.height - scrollerDimensions.height) / 2;
+      }
+    },
+    { eager: false }
+  );
+}
