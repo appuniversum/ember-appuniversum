9d5ced4c1db4c4742633b9e73f6d6e3ec79a6c2b
Karel Persoons <karel@brenner.company>
2023-01-04 22:42:05 +0100

    Move floating-ui modifier test to private folder, rename identifier & rework import

--- tests/integration/private/modifiers/floating-ui-test.js
+++ tests/integration/private/modifiers/floating-ui-test.js
@@ -3,13 +3,17 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Modifier | au-floating-ui', function (hooks) {
+import FloatingUiModifier from '@appuniversum/ember-appuniversum/private/modifiers/floating-ui';
+
+module('Integration | Private Modifier | floating-ui', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
+    this.set('floatingUi', FloatingUiModifier);
+
     await render(hbs`
       <button id="reference">Reference</button>
-      <div data-floater {{au-floating-ui "#reference"}}>Floater</div>
+      <div data-floater {{this.floatingUi "#reference"}}>Floater</div>
     `);
 
     assert.dom('[data-floater]').hasStyle({
