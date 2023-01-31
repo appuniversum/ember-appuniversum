1b5519a429fd8341c4cf666145d08a8c3490dbf9
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-01-31 11:10:53 +0100

    Make the au-link-to-models helper truly private
    By moving it into a private folder and removing the app re-export, it is very clear that this helper shouldn't be used outside of this addon itself.

--- addon/components/au-navigation-link.js
+++ addon/components/au-navigation-link.js
@@ -1,7 +1,10 @@
+import linkToModels from '@appuniversum/ember-appuniversum/private/helpers/link-to-models';
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class AuNavigationLink extends Component {
+  linkToModels = linkToModels;
+
   // this is a workaround for https://github.com/emberjs/ember.js/issues/19693
   // don't remove until we drop support for ember 3.27 and 3.28
   get queryParams() {
--- tests/unit/helpers/au-link-to-models-test.js
+++ tests/unit/helpers/au-link-to-models-test.js
@@ -1,13 +1,13 @@
-import { auLinkToModels } from '@appuniversum/ember-appuniversum/helpers/au-link-to-models';
+import { linkToModels } from '@appuniversum/ember-appuniversum/private/helpers/link-to-models';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Helper | au-link-to-models', function (hooks) {
+module('Unit | Helper | Private | link-to-models', function (hooks) {
   setupTest(hooks);
 
   test('it supports passing in a model as the first positional argument', function (assert) {
     let modelId = 1;
-    let result = auLinkToModels([modelId]);
+    let result = linkToModels([modelId]);
 
     assert.ok(Array.isArray(result));
     assert.strictEqual(result[0], 1);
@@ -15,7 +15,7 @@ module('Unit | Helper | au-link-to-models', function (hooks) {
 
   test('it supports passing in a models array as the second positional argument', function (assert) {
     let models = [1, 2, 3];
-    let result = auLinkToModels([undefined, models]);
+    let result = linkToModels([undefined, models]);
 
     assert.ok(Array.isArray(result));
     assert.strictEqual(result[0], 1);
@@ -25,14 +25,14 @@ module('Unit | Helper | au-link-to-models', function (hooks) {
 
   test('it passes through models as provided so the `<LinkTo>` component validation can be reused', function (assert) {
     let models = 1;
-    let result = auLinkToModels([undefined, models]);
+    let result = linkToModels([undefined, models]);
 
     assert.notOk(Array.isArray(result));
     assert.strictEqual(result, 1);
   });
 
   test('it returns an empty array if no model or models are provided', function (assert) {
-    let result = auLinkToModels([]);
+    let result = linkToModels([]);
 
     assert.ok(Array.isArray(result));
     assert.strictEqual(result.length, 0);
@@ -42,7 +42,7 @@ module('Unit | Helper | au-link-to-models', function (hooks) {
     let result;
 
     assert.throws(() => {
-      result = auLinkToModels([1, [1, 2]]);
+      result = linkToModels([1, [1, 2]]);
     });
 
     assert.notOk(result);
