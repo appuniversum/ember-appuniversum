4798eb168d3de72817feb78fac9d12eb2f9eeea0
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-02-20 17:04:10 +0100

    Only call .focus if the element exist
    This fixes an issue where the AuMainHeader component would trigger a type error exception if the `#main` element didn't exist.

--- addon/components/au-main-header.js
+++ addon/components/au-main-header.js
@@ -4,7 +4,6 @@ import { action } from '@ember/object';
 export default class AuMainHeader extends Component {
   @action
   headerLinkFocus() {
-    // Focus content window
-    document.querySelectorAll('#main')[0].focus();
+    document.querySelector('#main')?.focus();
   }
 }
