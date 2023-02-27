d4ca92b158df3b734fe75da662dc7c265ccaea88
Karel Persoons <karel@brenner.company>
2023-02-27 17:51:13 +0100

    Add deprecation warnings to legacy components

--- addon/components/au-control-checkbox.js
+++ addon/components/au-control-checkbox.js
@@ -1,7 +1,21 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { deprecate } from '@ember/debug';
 
 export default class AuControlCheckbox extends Component {
+  constructor() {
+    super(...arguments);
+
+    deprecate('[AuControlCheckbox] This component is deprecated in favour of AuCheckbox', false, {
+      id: '@appuniversum/ember-appuniversum.au-control-checkbox',
+      until: '3.0.0',
+      for: '@appuniversum/ember-appuniversum',
+      since: {
+        enabled: '2.5.0',
+      },
+    });
+  }
+
   get disabledClass() {
     if (this.args.disabled) return 'is-disabled';
     else return '';
