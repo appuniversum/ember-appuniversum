e751414228384d0f6296f91e710ed53c25ac1873
Elena Poelman <elena.poelman@redpencil.io>
2023-09-12 10:15:52 +0200

    remove unused iconPrefix getter in au-icon component

--- addon/components/au-icon.js
+++ addon/components/au-icon.js
@@ -1,13 +1,6 @@
 import Component from '@glimmer/component';
-import { getOwner } from '@ember/application';
 
 export default class AuButton extends Component {
-  get iconPrefix() {
-    const config = getOwner(this).resolveRegistration('config:environment');
-
-    return config.rootURL || '/';
-  }
-
   get size() {
     if (this.args.size == 'large') return 'au-c-icon--large';
     else return '';
