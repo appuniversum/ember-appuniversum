1ed60cfecec1c51e168c420db8c693055f452a95
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2023-10-09 15:27:54 +0200

    Convert the `AuMainHeader` component to .gjs
    This also allows us to inline the `AuNavigationNarrator` component.

--- addon/components/au-main-header.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class AuMainHeader extends Component {
-  @action
-  headerLinkFocus() {
-    document.querySelector('#main')?.focus();
-  }
-}
