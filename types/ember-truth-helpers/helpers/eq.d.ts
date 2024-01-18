28b75afae28d9518aeb726d8790657ea12bb883d
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-01-18 22:47:20 +0100

    Add temporary types for the `{{eq}}` helper
    v4 has official TS support, but some of our dependencies still use v3. This adds temporary types to make Glint happy. We can remove them once all our dependencies support v4, or by removing the usage in our own code.

---
+++ types/ember-truth-helpers/helpers/eq.d.ts
@@ -0,0 +1,2 @@
+// Temporary type since we still support ember-truth-helpers v3. v4 has its own types, so we can remove this when we drop v3 support.
+export default function eq(left: unknown, right: unknown): boolean;
