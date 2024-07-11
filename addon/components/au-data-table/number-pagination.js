74b9afb792b50d8e94e860c7dd399aa6fbc819e7
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-07-11 21:37:29 +0200

    Inline the ember-data-table components
    This copies over the ember-data-table .js code so we no longer need the addon itself. We were already overriding all the templates. This will make things easier to maintain and improve in the future.
    
    Linting errors are silenced with inline ignore comments. They will be fixed once we convert the code to .gts.

--- addon/components/au-data-table/number-pagination.js
+++ addon/components/au-data-table/number-pagination.js
@@ -1,7 +1,55 @@
-import NumberPagination from 'ember-data-table/components/number-pagination';
+/* eslint-disable ember/no-actions-hash, ember/no-classic-classes, ember/no-classic-components, ember/no-get, ember/require-tagless-components */
+import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { gt } from '@ember/object/computed';
 import { NavLeftIcon } from '../icons/nav-left';
 import { NavRightIcon } from '../icons/nav-right';
+
+// Source: https://github.com/mu-semtech/ember-data-table/blob/c690a3948b2d9d5f91d69f0a935c6b5cdb4862ca/addon/components/number-pagination.js
+const NumberPagination = Component.extend({
+  classNames: ['data-table-pagination'],
+  currentPage: computed('page', {
+    get() {
+      return this.page ? parseInt(this.page) + 1 : 1;
+    },
+    set(key, value) {
+      this.set('page', value - 1);
+      return value;
+    },
+  }),
+  firstPage: computed('links.first.number', function () {
+    return this.get('links.first.number') || 1;
+  }),
+  lastPage: computed('links.last.number', function () {
+    const max = this.get('links.last.number') || -1;
+    return max ? max + 1 : max;
+  }),
+  isFirstPage: computed('firstPage', 'currentPage', function () {
+    return this.firstPage == this.currentPage;
+  }),
+  isLastPage: computed('lastPage', 'currentPage', function () {
+    return this.lastPage == this.currentPage;
+  }),
+  hasMultiplePages: gt('lastPage', 0),
+  startItem: computed('size', 'currentPage', function () {
+    return this.size * (this.currentPage - 1) + 1;
+  }),
+  endItem: computed('startItem', 'nbOfItems', function () {
+    return this.startItem + this.nbOfItems - 1;
+  }),
+  pageOptions: computed('firstPage', 'lastPage', function () {
+    const nbOfPages = this.lastPage - this.firstPage + 1;
+    return Array.from(
+      new Array(nbOfPages),
+      (val, index) => this.firstPage + index,
+    );
+  }),
+  actions: {
+    changePage(link) {
+      this.set('page', link['number'] || 0);
+    },
+  },
+});
 
 export default NumberPagination.extend({
   tagName: '',
--- addon/components/au-data-table/th-sortable.js
+++ addon/components/au-data-table/th-sortable.js
@@ -1,7 +1,66 @@
-import ThSortable from 'ember-data-table/components/th-sortable';
+/* eslint-disable ember/no-actions-hash, ember/no-classic-classes, ember/no-classic-components, ember/require-tagless-components */
+import Component from '@ember/component';
+import { computed } from '@ember/object';
 import { NavDownIcon } from '../icons/nav-down';
 import { NavUpIcon } from '../icons/nav-up';
 import { NavUpDownIcon } from '../icons/nav-up-down';
+
+// Source: https://github.com/mu-semtech/ember-data-table/blob/c690a3948b2d9d5f91d69f0a935c6b5cdb4862ca/addon/components/th-sortable.js
+const ThSortable = Component.extend({
+  tagName: 'th',
+  classNames: ['sortable'],
+  classNameBindings: ['isSorted:sorted'],
+  dasherizedField: computed('field', function () {
+    return this.field.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+  }),
+  /**
+      Inverses the sorting parameter
+      E.g. inverseSorting('title') returns '-title'
+           inverseSorting('-title') returns 'title'
+  */
+  _inverseSorting(sorting) {
+    if (sorting.substring(0, 1) === '-') {
+      return sorting.substring(1);
+    } else {
+      return '-' + sorting;
+    }
+  },
+  isSorted: computed('dasherizedField', 'currentSorting', function () {
+    return (
+      this.currentSorting === this.dasherizedField ||
+      this.currentSorting === this._inverseSorting(this.dasherizedField)
+    );
+  }),
+  order: computed('dasherizedField', 'currentSorting', function () {
+    if (this.currentSorting === this.dasherizedField) {
+      return 'asc';
+    } else if (this.currentSorting === `-${this.dasherizedField}`) {
+      return 'desc';
+    } else {
+      return '';
+    }
+  }),
+
+  actions: {
+    /**
+       Sets the current sorting parameter.
+       Note: the current sorting parameter may contain another field than the given field.
+       In case the given field is currently sorted ascending, change to descending.
+       In case the given field is currently sorted descending, clean the sorting.
+       Else, set the sorting to ascending on the given field.
+     */
+    inverseSorting() {
+      if (this.order === 'asc') {
+        this.set('currentSorting', this._inverseSorting(this.currentSorting));
+      } else if (this.order === 'desc') {
+        this.set('currentSorting', '');
+      } else {
+        // if currentSorting is not set to this field
+        this.set('currentSorting', this.dasherizedField);
+      }
+    },
+  },
+});
 
 export default ThSortable.extend({
   NavDownIcon,
