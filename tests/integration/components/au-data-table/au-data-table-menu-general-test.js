dd97560fe1e7c91f7a813d38312bc6656245e58a
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-07-19 11:09:50 +0200

    Add the data-table tests
    This copies over the data table tests that aren't empty and does the bare minimum to make them pass with our custom templates.

---
+++ tests/integration/components/au-data-table/au-data-table-menu-general-test.js
@@ -0,0 +1,44 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module(
+  'Integration | Component | au-data-table-menu-general',
+  function (hooks) {
+    setupRenderingTest(hooks);
+
+    test('it renders', async function (assert) {
+      // Set any properties with this.set('myProperty', 'value');
+      // Handle any actions with this.on('myAction', function(val) { ... });
+
+      await render(hbs`{{au-data-table-menu-general}}`);
+
+      assert.dom('*').hasText('');
+    });
+
+    test('it renders block only if data table selection is empty', async function (assert) {
+      // Set any properties with this.set('myProperty', 'value');
+      // Handle any actions with this.on('myAction', function(val) { ... });
+
+      this.set('data-table', { selectionIsEmpty: true });
+      // Template block usage:
+      await render(hbs`
+      {{#au-data-table-menu-general data-table=this.data-table}}
+        template block text
+      {{/au-data-table-menu-general}}
+    `);
+      assert.dom('*').hasText('template block text');
+
+      this.set('data-table', { selectionIsEmpty: false });
+      // Template block usage:
+      await render(hbs`
+      {{#au-data-table-menu-general data-table=this.data-table}}
+        template block text
+      {{/au-data-table-menu-general}}
+    `);
+
+      assert.dom('*').hasText('');
+    });
+  },
+);
