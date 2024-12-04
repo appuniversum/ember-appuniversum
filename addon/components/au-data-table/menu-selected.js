2cbef9d6e05d1345af6a301c4d6bd29939ce8369
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-12-04 09:25:55 +0100

    Remove `ember-composable-helpers`
    We don't really need it, and the few couple of helpers we use can be easily implemented as a private helper where needed.

--- addon/components/au-data-table/menu-selected.js
+++ addon/components/au-data-table/menu-selected.js
@@ -1,10 +1,10 @@
 /* eslint-disable ember/no-classic-classes, ember/no-classic-components, ember/no-get, ember/require-tagless-components */
-import { action } from '@ember/object';
+import { action, computed } from '@ember/object';
 import { reads } from '@ember/object/computed';
 import Component from '@ember/component';
 
 // Source: https://github.com/mu-semtech/ember-data-table/blob/c690a3948b2d9d5f91d69f0a935c6b5cdb4862ca/addon/components/data-table-menu-selected.js
-export default Component.extend({
+const MenuSelected = Component.extend({
   init: function () {
     this._super(...arguments);
     this.set('data-table.enableSelection', true);
@@ -12,5 +12,11 @@ export default Component.extend({
   selectionCount: reads('data-table.selection.length'),
   clearSelection: action(function () {
     this.get('data-table').clearSelection();
+  }),
+});
+
+export default MenuSelected.extend({
+  selection: computed('data-table.selection.[]', function () {
+    return this.get('data-table.selection').slice();
   }),
 });
